2d3f23ddf4cfc5b21d365d4f187d5b417bf5e4fd
kinggencha <kinggencha@3dd2499b-01c5-0ce7-1336-b538894ed765>
2010-09-15 18:39:40 +0000

    Some cleanup in CLI entry point.

--- parseSdml.php
+++ parseSdml.php
@@ -1,6 +1,7 @@
 <?php
+  // This logging framework is part of the project SDML originated from.
+  // These references remain in the code to ensure compatibility with it.
   define( "SDML_USE_LOGGING", false );
-
   if( SDML_USE_LOGGING ) {
     require_once( dirname( __FILE__ ) . "/../../logging/Logging.php" );
     Logging::setLogLevel( Logging::eDEBUG );
@@ -16,9 +17,9 @@
   //$shortopts .= "o::";  // Output filename (if none is given stdout will be used)
   $shortopts .= "d";    // Debug output
 
-  $sdmlFilename = "";
-  $mysqlPassword = "";
-  $isDebug = false;
+  $sdmlFilename   = "";
+  $mysqlPassword  = "";
+  $isDebug        = false;
 
   $options = getopt( $shortopts );
   if( !isset( $options[ "i" ] ) ) {
@@ -60,10 +61,18 @@
     exit( $ex->getMessage() . "\n" );
   }
 
+  /**
+  * Print the command line parameters of this script.
+  */
   function printUseage() {
     echo( "parseSdml.php -i<input.sdml> [-p<MySql root password>] [-d]\n" );
   }
 
+  /**
+  * The callback to use to process SQL statements.
+  *
+  * @param string $query The query to process
+  */
   function queryCallback( $query ) {
     global $useMySqlConnector;
     global $isDebug;
@@ -78,15 +87,30 @@
     }
   }
 
+  /**
+  * Prints the supplied query to the screen.
+  *
+  * @param string $query
+  */
   function echoQuery( $query ) {
     echo $query . "\n";
   }
 
+  /**
+  * Passes the supplied query to a database connector.
+  *
+  * @param string $query
+  */
   function connectorQuery( $query ) {
     global $connector;
     $connector->processParserOutput( $query );
   }
 
+  /**
+  * Used for logging debug messages.
+  *
+  * @param string $message
+  */
   function debug( $message ) {
     if( SDML_USE_LOGGING ) {
       Logging::debug( $message );
@@ -95,6 +119,11 @@
     }
   }
 
+  /**
+  * Used for logging error messages.
+  *
+  * @param string $message
+  */
   function error( $message ) {
     if( SDML_USE_LOGGING ) {
       Logging::error( $message );
